e123c496ee666586a61883292ab63c7424870905
Qweyke <kettenkrad1337@yandex.ru>
2026-02-18 20:11:31 +0500

    feat: rework hamming code and implement SECDED feature

--- lab_2/hamming_code.py
+++ lab_2/hamming_code.py
@@ -5,103 +5,100 @@ class HammingCoder:
     def __init__(self, data_bits_len, secded=False) -> None:
         self.secded = secded
 
-        self._data_bits_len = data_bits_len
-        self._parity_bits_len = (
-            data_bits_len + 1
-        ).bit_length()  # Estimation, given total_bits ~= data_bits + redundant_bits
+        self._data_bits_num = data_bits_len
+        self._parity_bits_num = 0
 
-        # Find best parity len
-        while 2**self._parity_bits_len < (data_bits_len + self._parity_bits_len + 1):
-            self._parity_bits_len += 1
+        self._evaluate_parity_bits_needed()
 
-        self._block_len = self._data_bits_len + self._parity_bits_len
+        self._block_len = (
+            self._data_bits_num + self._parity_bits_num + 1
+        )  # +1 for SECDED feature
 
-        self._is_power_of_two = lambda i: ((i - 1) & i) == 0
+        print(f"Data len: {data_bits_len}, redundant len: {self._parity_bits_num}")
 
-        print(f"Data len: {data_bits_len}, redundant len: {self._parity_bits_len}")
+    def _evaluate_parity_bits_needed(self):
+        # (2^par ≥ total + par + 1)
+        while 2**self._parity_bits_num < (
+            self._data_bits_num + self._parity_bits_num + 1
+        ):
+            self._parity_bits_num += 1
 
-    # def encode_message(self, data_sequence: str):
-    #     if isinstance(data_sequence, str):
-    #         if data_sequence.startswith("0b"):
-    #             data_bits = BitArray(data_sequence)
+    def _is_power_of_two(self, num):
+        return (num - 1) & num == 0
 
-    #         else:
-    #             data_bits = BitArray(bytes=data_sequence.encode("utf-8"))
+    def _covered_data_bits_positions(self, parity_bit):
+        for pos in range(3, self._block_len, 1):
+            if not self._is_power_of_two(pos) and (pos & parity_bit):
+                yield pos
 
-    #     padding_bits = self._data_bits_len - (data_bits.len() % self._data_bits_len)
-    #     padded_data: BitArray = data_bits + BitArray(length=padding_bits)
+    def encode_block(self, input_data: BitArray):
+        encoded = BitArray(self._block_len)
 
-    #     for i in range(0, len(padded_data), self._data_bits_len):
-    #         chunk = padded_data[i : i + self._data_bits_len]
-    #         self.encode_block(chunk)
+        # Fill in the data bits
+        data_idx = 0
+        for pos in range(1, self._block_len):
+            if not self._is_power_of_two(pos):
+                encoded[pos] = input_data[data_idx]
+                data_idx += 1
 
-    def encode_block(self, bit_sequence: BitArray):
-        code_block = BitArray(length=self._block_len)
+        # Fill in parity bits
+        for power in range(self._parity_bits_num):
+            parity_bit_pos = 1 << power
 
-        # Fill code block
-        block_pos = 0
-        for i in range(1, self._block_len + 1):
-            # If not power of 2, then insert data bit
-            if not self._is_power_of_two(i):
-                code_block[i - 1] = bool(bit_sequence[block_pos])
-                block_pos += 1
+            xor_parity_res = 0  # Gives bit-value to achieve parity
+            for data_pos in self._covered_data_bits_positions(parity_bit_pos):
+                xor_parity_res ^= encoded[data_pos]
+            encoded[parity_bit_pos] = bool(xor_parity_res)
 
-        # Flip the query bits to achieve parity, if needed
+        if self.secded:
+            encoded[0] = bool(encoded.count(True) % 2)
+            return encoded
+        else:
+            return encoded[1:]
+
+    def decode_block(self, encoded: BitArray):
+        encoded = (
+            encoded if self.secded else (BitArray([0]) + encoded)
+        )  # For easy indexing
+
+        # Find error position
         xor_syndrome = 0
-        for i in range(1, self._block_len + 1):
-            if code_block[i - 1] != 1:
-                continue
+        for pos in range(1, self._block_len):
+            if encoded[pos] == True:
+                xor_syndrome ^= pos
 
-            xor_syndrome ^= i
+        overall_error = encoded.count(True) % 2
 
-        for i in range(self._parity_bits_len):
-            redundant_pos = 1 << i
+        if xor_syndrome != 0:
+            if self.secded and not overall_error:
+                print("SECDED: Double error detected")
+                return None
 
-            if redundant_pos & xor_syndrome:
-                code_block[redundant_pos - 1] = True
+            else:
+                encoded.invert(xor_syndrome)
+                print(f"HAMMING: Error found and corrected at position {xor_syndrome}")
 
-        # if self.secded:
-        #     global_parity = code_block.count(True) % 2
-        #     code_block.append([global_parity])
+        output_data = BitArray()
+        for pos in range(1, self._block_len):
+            if not self._is_power_of_two(pos):
+                output_data.append([encoded[pos]])
 
-        return code_block
-
-    def decode_block(self, code_block: BitArray):
-        # Find error pos
-        xor_syndrome = 0
-        for i in range(1, self._block_len + 1):
-            if code_block[i - 1] != 1:
-                continue
-
-            xor_syndrome ^= i
-
-        # Change bit at error pos, if error is found
-        if xor_syndrome > 0:
-            if code_block[xor_syndrome]:
-                code_block.invert(xor_syndrome - 1)
-
-        # Extract data bits
-        data_sequence = BitArray()
-        data_pos = 0
-        for i in range(1, self._block_len + 1):
-            # If not power of 2, then extract data bit
-            if not self._is_power_of_two(i):
-                data_sequence.append([code_block[i - 1]])
-                data_pos += 1
-
-        return data_sequence[: self._data_bits_len]
+        return output_data
 
 
 if __name__ == "__main__":
-
     test = BitArray("0b00110001110")
-    coder = HammingCoder(data_bits_len=len(test))
+
+    coder = HammingCoder(data_bits_len=len(test), secded=True)
     print(f"Original block: {test.bin}")
 
     block_encoded = coder.encode_block(test)
     print(f"Original encoded block: {block_encoded.bin}")
-    block_encoded.invert(2)
+
+    block_encoded.invert(3)
+    # block_encoded.invert(7)
     print(f"Inverted encoded block: {block_encoded.bin}")
 
     block_decoded = coder.decode_block(block_encoded)
-    print(f"Decoded block: {block_decoded}")
+    if block_decoded:
+        print(f"Decoded block: {block_decoded.bin}")
