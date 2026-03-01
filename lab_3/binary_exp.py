42d9bdf9bded9208d8f9b0632863d360de96cad3
Qweyke <qweyke@gmail.com>
2026-03-01 21:44:39 +0500

    feat: complete prime-num-generator module

--- lab_3/binary_exp.py
+++ lab_3/binary_exp.py
@@ -9,5 +9,16 @@ def bin_exp(base, power):
     return res
 
 
+def mod_bin_exp(base, power, mod):
+    res = 1
+    while power > 0:
+        if power & 1:
+            res = (res * base) % mod
+        base = (base * base) % mod
+        power >>= 1
+
+    return res
+
+
 if __name__ == "__main__":
     print(bin_exp(24, 3))
--- lab_3/prime_generator.py
+++ lab_3/prime_generator.py
@@ -1,12 +1,14 @@
 import secrets
 
+from binary_exp import mod_bin_exp
+
 
 class PrimeNumberGenerator:
     def __init__(self, num_len=1024) -> None:
         self.num_len = num_len
-        self._primes = self._sieve_of_eratosthenes()
+        self._primes = self._generate_primes_by_sieve_of_eratosthenes()
 
-    def _sieve_of_eratosthenes(self, limit=1000):
+    def _generate_primes_by_sieve_of_eratosthenes(self, limit=1000):
         prime_nums = []
         raw_nums = [True] * (limit + 1)  # +1 for iterating from idx = 2
         sqrt_limit = int(
@@ -28,23 +30,57 @@ class PrimeNumberGenerator:
 
         return prime_nums
 
-    def _generate_odd_bit_candidate(self):
-        bit_num = secrets.randbits(self.num_len)
-        # Make sure the MSB is 1
-        bit_num |= 1 << (self.num_len - 1)
-        # Make the number odd
-        bit_num |= 1
+    def _is_prime_by_miller_rabin(self, odd_n, check_iters=40):
+        def factorize_by_powers_of_two(even_num):
+            d = even_num
+            s = 0
+            # Check if candidate num can be divided by 2
+            while not (d & 1):
+                d >>= 1  # Factorize by 2
+                s += 1
 
-        return bit_num
+            # Return max power of 2 and remaining odd factor 'd'
+            return s, d
 
-    def _is_prime_by_miller_rabin(self, candidate, check_iters=40):
-        for i in range(check_iters):
-            base = secrets.randbelow(candidate - 4) + 2
+        power_s, odd_d = factorize_by_powers_of_two(odd_n - 1)
+
+        # Test n-candidate for primality
+        for _ in range(check_iters):
+            # Get random 'a-base from [2, n - 2]
+            a_base = secrets.randbelow(odd_n - 4) + 2
+
+            # Check odd start-number-'d'
+            mod_res = mod_bin_exp(a_base, odd_d, odd_n)
+            if mod_res == 1 or mod_res == odd_n - 1:
+                # 'n'-candidate passed check, all the other mod results will be = 1
+                continue
+
+            # Check for last power 's' is obsolete, if we didn't achieve mod_res == n - 1, then
+            # next mod_res isn't 1, so Ferma Little theorem doesn't hold
+            entry_found = False
+            for _ in range(power_s - 1):
+                mod_res = mod_bin_exp(mod_res, 2, odd_n)
+                if mod_res == odd_n - 1:
+                    entry_found = True
+                    break
+
+            if not entry_found:
+                return False
+
+        return True
 
     def generate(self):
+        def generate_odd_bit_candidate():
+            bit_num = secrets.randbits(self.num_len)
+            # Make sure the MSB is 1
+            bit_num |= 1 << (self.num_len - 1)
+            # Make the number odd
+            bit_num |= 1
+            return bit_num
+
         while True:
             # 0. Generate odd candidate
-            candidate = self._generate_odd_bit_candidate()
+            candidate = generate_odd_bit_candidate()
 
             # 1. Light check by small primes
             passed_small_prime_check = True
